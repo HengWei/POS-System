d667bed5b279b8a3bda1e40fbf592ea6b78fe5f3
Duck <swatduck@gmail.com>
2017-05-29 08:35:54 +0800

    1. 修正資料由網頁帶入 非測試資料

--- POSSys/views.py
+++ POSSys/views.py
@@ -3,14 +3,14 @@ from POSSys.models import Menu, Sell
 from datetime import datetime
 from django.utils import timezone
 
+
 # Create your views here.
 
 def index(request):
     menuList = Menu.objects.filter(menuparentid=0)
     menu1 = Menu.objects.all().exclude(menuparentid=0)
     if request.method == 'POST':
-        data = ['101', '錫蘭紅茶', '25', '1', '25']
-        # data = request.POST['sellList']
+        data = request.POST['sellList'].split(',')
         dataSell = Sell()
         for idx, val in enumerate(data):
             if (idx) % 5 == 0:
@@ -19,10 +19,9 @@ def index(request):
                 dataSell.sellquantity = val
             if (idx) % 5 == 4:
                 dataSell.sellbasicid = 0
-                dataSell.selldatetime= datetime.now(timezone.utc).astimezone()
+                dataSell.selldatetime = datetime.now(timezone.utc).astimezone()
                 dataSell.save()
                 dataSell.clean()
-
     return render(request, 'index.html', locals())
 
 
